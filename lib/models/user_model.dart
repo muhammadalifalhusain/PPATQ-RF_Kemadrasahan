d28bdf7aa9ecc767341fa13b3d0a2375494a31d3
muhammadalifalhusain <muhalifalhusain@gmail.com>
2026-01-23 23:17:08 +0700

    update : model users

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,7 +1,10 @@
+import '../utils/model_helper.dart';
+
 class Users {
   final int idUser;
   final String nama;
   final String? photo;
+  final bool isWaliKelas;
   final String accessToken;
   final int expiresIn;
 
@@ -9,17 +12,21 @@ class Users {
     required this.idUser,
     required this.nama,
     this.photo,
+    required this.isWaliKelas,
     required this.accessToken,
     required this.expiresIn,
   });
 
   factory Users.fromJson(Map<String, dynamic> json) {
+    final data = json['data'] ?? json;
+
     return Users(
-      idUser: json['id'],
-      nama: json['nama'],
-      photo: json['photo'],
-      accessToken: json['accesToken'],
-      expiresIn: json['expiresIn'],
+      idUser: ModelHelper.safeInt(data['id']),
+      nama: ModelHelper.safeString(data['nama']) ?? '',
+      photo: ModelHelper.safeString(data['photo']),
+      isWaliKelas: data['isWaliKelas'] ?? false,
+      accessToken: ModelHelper.safeString(data['accesToken']) ?? '',
+      expiresIn: ModelHelper.safeInt(data['expiresIn']),
     );
   }
 }
